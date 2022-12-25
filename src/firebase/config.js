920dba5b851e65166a8c336549b359add38befbf
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2022-12-25 23:46:16 +0200

    added firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyDN9MO_NSwj_9V-oFAiIBpBpG9HvnJ5rk8",
+  authDomain: "live-chat-378cb.firebaseapp.com",
+  projectId: "live-chat-378cb",
+  storageBucket: "live-chat-378cb.appspot.com",
+  messagingSenderId: "79484589135",
+  appId: "1:79484589135:web:917a2b138ecc2180d8863a",
+  measurementId: "G-DQ3SXP7D98",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+const projectFireStore = firebase.firestore();
+const timeStamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFireStore, timeStamp };
