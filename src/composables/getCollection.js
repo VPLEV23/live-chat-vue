89883580a964ba90b5df9f7a4cdd6d5f264f1a90
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2022-12-26 13:18:09 +0200

    Real-Time Firestore Listener

---
+++ src/composables/getCollection.js
@@ -0,0 +1,33 @@
+import { ref } from "vue";
+import { projectFirestore } from "../firebase/config";
+
+const getCollection = (collection) => {
+  const documents = ref(null);
+  const error = ref(null);
+
+  let collectionRef = projectFirestore
+    .collection(collection)
+    .orderBy("createdAt");
+
+  collectionRef.onSnapshot(
+    (snap) => {
+      let results = [];
+      snap.docs.forEach((doc) => {
+        doc.data().createdAt && results.push({ ...doc.data(), id: doc.id });
+      });
+
+      // update values
+      documents.value = results;
+      error.value = null;
+    },
+    (err) => {
+      console.log(err.message);
+      documents.value = null;
+      error.value = "could not fetch the data";
+    }
+  );
+
+  return { error, documents };
+};
+
+export default getCollection;
