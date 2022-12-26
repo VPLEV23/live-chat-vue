e034bea7efcd89de1efe7bfadf692ce89f301bfe
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2022-12-26 11:43:39 +0200

    Navbar Component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <div>
+      <p>Hey.... there display name here</p>
+      <p class="email">Currently login in as.... email</p>
+    </div>
+    <button>LogOut</button>
+  </nav>
+</template>
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style>
+nav {
+  padding: 20px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+nav p {
+  margin: 2px auto;
+  font-size: 16px;
+  color: #444;
+}
+nav p.email {
+  font-size: 14px;
+  color: #999;
+}
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>Chatroom</div>
+  <div class="container">
+    <NavBar />
+  </div>
 </template>
 
 <script>
+import NavBar from "../components/NavBar.vue";
 export default {
   name: "Chatroom",
+  components: {
+    NavBar,
+  },
 };
 </script>
