6d5dbbd3c8067f54cca7134a112dd957ff17e36d
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2022-12-26 12:39:59 +0200

    Watching User

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,11 +5,24 @@
 </template>
 
 <script>
+import { watch } from "vue";
+import { useRouter } from "vue-router";
 import NavBar from "../components/NavBar.vue";
+import getUser from "../composables/getUser";
+
 export default {
   name: "Chatroom",
   components: {
     NavBar,
+  },
+  setup() {
+    const { user } = getUser();
+    const router = useRouter();
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "Welcome" });
+      }
+    });
   },
 };
 </script>
