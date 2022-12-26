37ac5e60009a13564795499eba7076f79ad2696c
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2022-12-26 12:15:14 +0200

    Get Current USer

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,20 @@
 <template>
-  <nav>
+  <nav v-if="user">
     <div>
-      <p>Hey.... there display name here</p>
-      <p class="email">Currently login in as.... email</p>
+      <p>Hey.... there {{ user.displayName }}</p>
+      <p class="email">Currently login in as{{ user.email }}</p>
     </div>
     <button @click="handleClick">LogOut</button>
   </nav>
 </template>
 <script>
 import useLogout from "../composables/useLogout";
+import getUser from "../composables/getUser";
 export default {
   name: "NavBar",
   setup() {
     const { logout, error } = useLogout();
+    const { user } = getUser();
 
     const handleClick = async () => {
       await logout();
@@ -20,7 +22,7 @@ export default {
         console.log("user log out");
       }
     };
-    return { handleClick };
+    return { handleClick, user };
   },
 };
 </script>
