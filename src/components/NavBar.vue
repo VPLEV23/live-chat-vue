eb9c45c4148078f419917250e4b23dbe51551c13
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2022-12-26 12:04:35 +0200

    Logging User Out

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,12 +4,24 @@
       <p>Hey.... there display name here</p>
       <p class="email">Currently login in as.... email</p>
     </div>
-    <button>LogOut</button>
+    <button @click="handleClick">LogOut</button>
   </nav>
 </template>
 <script>
+import useLogout from "../composables/useLogout";
 export default {
   name: "NavBar",
+  setup() {
+    const { logout, error } = useLogout();
+
+    const handleClick = async () => {
+      await logout();
+      if (!error.value) {
+        console.log("user log out");
+      }
+    };
+    return { handleClick };
+  },
 };
 </script>
 
